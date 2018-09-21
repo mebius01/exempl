8f4b73b0b875897535e3afba94a3b9db98aea61b
iv <consmebius@gmail.com>
2018-09-21 21:19:54 +0300

    add def slugify and new_post read in BD

--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render, get_object_or_404
-from django.views.generic import DetailView
+# from django.views.generic import DetailView
 from django.http import HttpResponseRedirect
 from blog.models import Post
 from blog.forms import PostForm, UserLoginForm
-from django.template.defaultfilters import slugify
-from django.contrib import auth
-from django.shortcuts import redirect
+from pytils.translit import slugify
+# from django.contrib import auth
+# from django.shortcuts import redirect
 
 
 
@@ -29,31 +29,11 @@ def about_blog(request):
 def new_post(request):
 	if request.method == 'POST':
 		form = PostForm(request.POST, request.FILES)
+		author = form.save()
+		author.slug = slugify(author.title)
 		if form.is_valid():
 			form.save()
 			return render(request, 'blog/new_post', {'complite': "complite"})
-			# return render(request, 'blog/new_post', {'form': form, 'complite': "complite"})
-
-
-# Post.objects.order_by('-publish')[:1].get()
-# Примечание
-
-# Поля, которые не определены в форме, не будут учитываться при вызове метода save(). Также, если вы вручную добавите в форму исключенные поля, то они не будут заполняться из экземпляра модели.
-
-# Django будет препятствовать всем попыткам сохранить неполную модель. Таким образом, если модель требует заполнения определённых полей и для них не предоставлено значение по умолчанию, то сохранить форму для такой модели не получится. Для решения этой проблемы вам потребуется создать экземпляр такой модели, передав ему начальные значения для обязательных, но незаполненных полей:
-
-# author = Author(title='Mr')
-# form = PartialAuthorForm(request.POST, instance=author)
-# form.save()
-
-# В качестве альтернативы, вы можете использовать save(commit=False) и вручную определить все необходимые поля:
-
-# form = PartialAuthorForm(request.POST)
-# author = form.save(commit=False)
-# author.title = 'Mr'
-# author.save()
-
-Обратитесь к разделу section on saving forms для подробностей по использованию save(commit=False).
 	else:
 		form = PostForm()
 	return render(request, 'blog/new_post', {'form': form})
